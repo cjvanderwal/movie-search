a047456d39ab6bd886d9e7cdda68ee6f21781235
cameron <cjvanderwal@wirelessprvnat-172-17-192-192.near.illinois.edu>
2016-03-06 19:02:33 -0600

    finished mp3

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,21 @@
 var app = angular.module('mp3',['ngRoute']);
 
-app.config(function ($routeProvider) {
-
-})
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/List', {
+        templateUrl: 'partials/list.html',
+        controller: 'ListController'
+      }).
+      when('/Gallery', {
+        templateUrl: 'partials/gallery.html',
+        controller: 'GalleryController'
+      }).
+      when('/Details/:imdbID', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailsController'
+      }).
+      otherwise({
+        redirectTo: '/List'
+      });
+}]);
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,37 @@
-/* Sample Controller */
+app.controller('ListController', function($scope, $http) {
+  // $scope.movieSearch= "Search for movies";
+  $scope.sortOptions = ["title", "rank"];
+	$scope.currOption = "";
+
+  // get the list of movies from the JSON
+  $http.get("data/imdb250.json").then(function(response) {
+    $scope.movieList = response.data;
+  });
+
+  // find the listed titles by the search string
+  // $scope.movieFilter = function(movie) {
+  //   return function(movie) {
+  //     if ($scope.movieSearch === null) {
+  //       return false;
+  //     }
+  //     return movie.title.search($scope.movieSearch) != -1
+  //   };
+  // };
+});
+
+app.controller('DetailsController', function($scope, $http, $routeParams) {
+  $scope.currMovie = $routeParams.imdbID;
+
+  // get the list of movies from the JSON
+  $http.get("data/imdb250.json").then(function(response) {
+    $scope.movieList = response.data;
+  });
+});
+
+app.controller('GalleryController', function($scope, $http) {
+
+  // get the list of movies from the JSON
+  $http.get("data/imdb250.json").then(function(response) {
+    $scope.movieList = response.data;
+  });
+});
